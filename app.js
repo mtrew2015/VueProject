8468bd3de84f7fbcbad9959f83b17bf40257f1e2
Michael Trew <40902786+mtrew2015@users.noreply.github.com>
2020-03-16 21:59:30 -0400

    adding functionality

--- app.js
+++ app.js
@@ -1,8 +1,49 @@
 new Vue({
-    el: '#app', 
-    data: {
-        playerHealth: 100, 
-        monsterHealth: 100, 
-        gameIsRunning: false, 
-    }
-})
+	el: '#app',
+	data: {
+		playerHealth: 100,
+		monsterHealth: 100,
+		gameIsRunning: false
+	},
+	methods: {
+		startGame: function() {
+			this.gameIsRunning = true;
+			this.playerHealth = 100;
+			this.monsterHealth = 100;
+		},
+		attack: function() {
+			this.monsterHealth -= this.calculateDamage(3, 10);
+			if (this.checkWin()) {
+				return;
+			}
+			this.playerHealth -= this.calculateDamage(5, 12);
+
+			this.checkWin();
+		},
+		specialAttack() {},
+
+		heal: function() {},
+		giveUp: function() {},
+		calculateDamage: function(min, max) {
+			return Math.max(Math.floor(Math.random() * max) + 1, min);
+		},
+		checkWin: function() {
+			if (this.monsterHealth <= 0) {
+				if (confirm('You Won!, New Game?')) {
+					this.startGame();
+				} else {
+					this.gameIsRunning = false;
+				}
+				return true;
+			} else if (this.playerHealth <= 0) {
+				if (confirm('You Lost! Play Again?')) {
+					this.startGame();
+				} else {
+					this.gameIsRunning = false;
+				}
+				return true;
+			}
+			return false;
+		}
+	}
+});
